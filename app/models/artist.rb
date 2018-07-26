5b530a3b6112b4be29b025544ad1d1c3a9d9c21e
Lorena <lorena.giddens@outlook.com>
2018-07-26 12:38:22 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,7 +9,7 @@ class Artist < ActiveRecord::Base
 
   def song_count
     #return the number of songs associated with the artist
-    self.sum(:songs)
+    self.count(:songs)
   end
 
   def genre_count
